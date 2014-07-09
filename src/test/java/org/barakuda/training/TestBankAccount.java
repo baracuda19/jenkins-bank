bcc17cfb43193392fd6f53ede8c22a51a3c00b27
ugur <ualtindag@live.com>
2014-07-09 05:08:55 -0400

    added asome test cases

--- src/test/java/org/barakuda/training/TestBankAccount.java
+++ src/test/java/org/barakuda/training/TestBankAccount.java
@@ -5,6 +5,33 @@ import junit.framework.TestSuite;
 import junit.framework.Assert;
 import org.junit;
  
-public class TestBankAccount {
- 
+
+public class TestBankAccount 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
 }

9369f225eeee2ebdaa8ee029d5172c5092973696
ugur <ualtindag@live.com>
2014-07-09 13:11:08 -0400

    fixed junit problem by updating junit version number

--- src/main/java/org/barakuda/training/BankAccount.java
+++ src/main/java/org/barakuda/training/BankAccount.java
@@ -1,5 +1,5 @@
 package org.barakuda.training;
-
+ 
 public class BankAccount {
  
   private double balance;
--- src/test/java/org/barakuda/training/TestBankAccount.java
+++ src/test/java/org/barakuda/training/TestBankAccount.java
@@ -1,34 +1,9 @@
 package org.barakuda.training;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+
 import junit.framework.Assert;
-import org.junit;
+import org.junit.Before;
+import org.junit.Test;
  
-
-public class TestBankAccount 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+public class TestBankAccount {
+ 
 }

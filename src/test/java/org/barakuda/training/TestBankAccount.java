7aaff242f68323c9bda7407a28f879073cbc7830
ugur <ualtindag@live.com>
2014-07-09 04:19:52 -0400

    initial commit

---
+++ src/test/java/org/barakuda/training/TestBankAccount.java
@@ -0,0 +1,12 @@
+package org.barakuda.training;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+ 
+public class TestBankAccount {
+ 
+}

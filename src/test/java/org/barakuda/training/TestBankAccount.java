76a2bbb7b2535e7d4def68f75100ec3cc1e84865
ugur <ualtindag@live.com>
2014-07-09 04:42:11 -0400

    changed test.java's import

--- src/test/java/org/barakuda/training/TestBankAccount.java
+++ src/test/java/org/barakuda/training/TestBankAccount.java
@@ -1,8 +1,5 @@
 package org.barakuda.training;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import junit.framework.Assert;
 import org.junit.Before;
 import org.junit.Test;
